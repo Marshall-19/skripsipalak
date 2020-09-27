ffd4d1f24714abaddac23307ad9825da902a7240
Hanif Marshal <hanifmarshall19@gmail.com>
2020-09-27 15:52:02 +0700

    surat

---
+++ application/views/surat/surat_domisi.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <center>
+        <font size="5"><u><b>SURAT KETERANGAN</b></u></font><br />Nomor:
+        145/5/IV/DS/2019
+    </center>
+    <br /><br /><br />
+    <font align="justify">
+        Yang bertandatangan di bawah ini , Kepala Desa Warungbambu Kecamatan Karawang Timur Kabupaten Karawang menerangkan :
+    </font>
+    <table width="100%">
+        <tr>
+            <td width="20%">Nama Lengkap</td>
+            <td width="3%">:</td>
+            <td width="77%">Muhamad Husein</td>
+        </tr>
+        <tr>
+            <td>NIK</td>
+            <td>:</td>
+            <td>3215260112990001</td>
+        </tr>
+        <tr>
+            <td>Tempat/Tanggal Lahir</td>
+            <td>:</td>
+            <td>Karawang / 04 December 1999</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>:</td>
+            <td>Laki Laki</td>
+        </tr>
+        <tr>
+            <td>Status Perkawinan</td>
+            <td>:</td>
+            <td>Belum Menikah</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>:</td>
+            <td>Belum Bkerja</td>
+        </tr>
+        <tr>
+            <td>Alamat KTP</td>
+            <td>:</td>
+            <td>Sukamaju ,Desa Warung Bambu , Kecamatan Karawang Timur Kota Karawan. 41371 </td>
+        </tr>
+    </table>
+    <font align="justify">
+        Berdasarkan Surat pengantar RT/RW setempat No. svsd, Bahwa yang bersangkutan benar
+        saat ini Berdomisili/Bertempat Tinggal di Sukamaju ,Desa Warung Bambu , Kecamatan Karawang Timur Kota Karawan. 41371 .
+        <br /><br />
+        Surat Keterangan ini berlaku: 25 November 2019 s/d
+        25 December 2019<br /><br />
+        Demikian Surat Keterangan ini dibuat dengan sebenarnya, untuk dapat dipergunakan sesuai keperluannya serta agar yang
+        berkepentingan menjadi maklum.<br /><br /><br />
+    </font>
+    <table width="100%">
+        <tr>
+            <td width="50%"></td>
+            <td width="50%">
+                <center>Warungbambu, 25 November 2019 </center>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <center>Yang Bersangkutan</center>
+            </td>
+            <td>
+                <center>Kepala Desa Warungbambu</center>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <center><b><u>Muhamad Husein</u></b></center>
+            </td>
+            <td>
+                <center><b><u>Selamet. S.Kom</u></b></center>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <center>NIP. 19640112 199203 1 008</center>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
---
+++ application/views/surat/surat_kelahiran.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <center><b>UNTUK YANG BERSANGKUTAN</b> <br />
+        <font size="5"><u><b>SURAT KETERANGAN KELAHIRAN</b></u></font><br />Nomor:
+        474.1/6/Desa/2020
+    </center>
+    <br /><br /><br />
+    <font align="justify">
+        Yang bertandatangan di bawah ini , Kepala Desa Desa Warungbambu Kecamatan
+        Karawang Timur Kabupaten Karawang, menerangkan bahwa pada:
+    </font>
+    <table width="100%">
+        <tr>
+            <td width="20%">Hari</td>
+            <td width="3%">:</td>
+            <td width="77%">Selasa</td>
+        </tr>
+        <tr>
+            <td>Tempat/Tanggal Lahir</td>
+            <td>:</td>
+            <td>Padang /
+                07 September 2020</td>
+        </tr>
+        <tr>
+            <td>Pukul</td>
+            <td>:</td>
+            <td>00:32</td>
+        </tr>
+    </table>
+    <br />
+    <center>Telah lahir seorang anak Laki-Laki Bernama
+        :<br /><b>"Marshall Hanif"</b><br /></center>
+    Dari seorang Ibu :<table width="100%">
+        <tr>
+            <td width="20%">Nama Lengkap</td>
+            <td width="3%">:</td>
+            <td width="77%">Muhamad Husein</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>:</td>
+            <td>Belum Bkerja</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>:</td>
+            <td>Sukamaju ,Desa Warung Bambu , Kecamatan Karawang Timur Kota Karawan. 41371 </td>
+        </tr>
+    </table> <br>
+    Dari seorang Ayah <br />
+    <table width="100%">
+        <tr>
+            <td width="20%">Nama Lengkap</td>
+            <td width="3%">:</td>
+            <td width="77%">Muhamad Husein</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>:</td>
+            <td>Belum Bkerja</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>:</td>
+            <td>Sukamaju ,Desa Warung Bambu , Kecamatan Karawang Timur Kota Karawan. 41371 </td>
+        </tr>
+    </table>
+    <br />Surat keterangan ini dibuat berdasarkan keterangan Pelapor :
+    <table width="100%">
+        <tr>
+            <td width="20%">Nama Lengkap</td>
+            <td width="3%">:</td>
+            <td width="77%">Muhamad Husein</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>:</td>
+            <td>Belum Bkerja</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>:</td>
+            <td>Sukamaju ,Desa Warung Bambu , Kecamatan Karawang Timur Kota Karawan. 41371 </td>
+        </tr>
+    </table>
+    <br />
+    <font align="justify">
+        Surat Keterangan ini di gunakan sebagaimana mestinya.<br /><br /><br />
+    </font>
+    <table width="100%">
+        <tr>
+            <td width="50%"></td>
+            <td width="50%">
+                <center>Warungbambu, 27 September 2020 </center>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <center>Kepala Desa Warungbambu</center>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <center><b><u>Selamet. S.Kom</u></b></center>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <center>NIP. 19640112 199203 1 008</center>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
